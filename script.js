33adcbe6418a955680e15db9aaf23680bde2cc83
Mark Ryan <markrryan99@gmail.com>
2023-06-21 18:50:52 -0700

    added side bar

--- script.js
+++ script.js
@@ -100,3 +100,24 @@ restartBtn.addEventListener('click', () => {
     return (currentPlayer === 1),
     document.getElementById('playerText').innerText = 'Click on a Spot to begin'
 });
+
+// Sidebar Javascript
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const sideMenu = document.querySelector('.side-menu');
+const burger = document.querySelectorAll('.hamburger')
+console.log(burger)
+
+hamburgerMenu.addEventListener('click', function() {
+  hamburgerMenu.classList.toggle('active');
+  sideMenu.classList.toggle('active');
+  // hamburgerMenu.style.display = 'none'
+});
+
+document.addEventListener('click', function(event) {
+  const target = event.target;
+  if (!target.closest('.side-menu-container')) {
+    hamburgerMenu.classList.remove('active');
+    sideMenu.classList.remove('active');
+    // hamburgerMenu.style.display = 'block'
+  }
+});
